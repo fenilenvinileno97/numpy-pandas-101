1b3ef812c7a0c90a247450d0a4c4f5b95ab25478
Marlon Marin <marlonmariin97@outlook.com>
2023-08-24 12:11:49 -0500

    feat pattern(word) for retrieving filenames and their encoding for several parameters through python3 char_detection.py arg1 arg2 ... feat try_reading_with_alternative_encodings(filepath) will find out an appropriate encoding type from encoded.py library, this is for get a better accuracy and diminishing execution time in dataset_encoding.py

--- Environment project/char_detection.py
+++ Environment project/char_detection.py
@@ -1,17 +1,26 @@
 #!usr/bin/env python3
 from chardet import detect
 import sys
+import re
 
 def char_type(filepath):
     with open(filepath, 'rb') as f:
         result = detect(f.read())
     return result['encoding']
 
+def pattern(word):
+    pattern = r"(\/\w+\.\w+$)"
+    result = re.search(pattern, word)
+    return result[0].replace("/", "")
+
 def run():
-    filepath = sys.argv[0]
-    # result = char_type(filepath)
-    # print(f"The ")
-    print(filepath)
+    filepath = sys.argv
+    result = {pattern(key):char_type(key) for key in filepath[1:]}
+    if len(result) > 0:
+        for key, value in result.items():
+            print(f"The encoding for file '{key}' is", value, sep=': ')
+    else:
+        print('Enter a parameter!')
     
 if __name__ == '__main__':
     run()
--- Environment project/dataset_encoding.py
+++ Environment project/dataset_encoding.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import sys
+from char_detection import char_type
 from encoded import encodings
 
-def read_csv_with_encoding(filepath, encoding):
-    try:
-        df = pd.read_csv(filepath, sep=',', header=0, encoding=encoding)
-        return df
-    except UnicodeDecodeError:
-        print(f"Error reading file with {encoding} encoding. Trying next encoding...")
+def reading_dataframe(filepath, encoding):
+    df = pd.read_csv(filepath, sep=',', header=0, encoding=encoding)
+    return df.iloc[:5, :5]
+
+def try_reading_with_alternative_encodings(filepath):
+    for encoding in encodings:
+        try:
+            df = reading_dataframe(filepath, encoding)
+            return (df, encoding)
+        except UnicodeDecodeError:
+            pass
+    return None
 
 def run():
-    file_path = '../../Datasets/cars/cars.csv'
-    encodings_list = encodings
-
-    for encoding in encodings_list:
-        df = read_csv_with_encoding(file_path, encoding)
-        if df is not None:
-            print(f"File successfully read with encoding: {encoding}")
-            # Perform operations on the DataFrame 'df' as needed
-            print(df.head())  # Example: print the first few rows of the DataFrame
-        if df is None:
-            print("Find out other appropiate encodings")
-            break
+    file_path = '../../Datasets/CO2_emission_by_countries.csv'
+    print(try_reading_with_alternative_encodings(file_path))
     
 if __name__ == '__main__':
     run()
